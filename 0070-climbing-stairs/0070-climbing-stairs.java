8683eface744c56b676326b33e8401cce095b4be
adityalaxkar <adityalaxkar9876@gmail.com>
2026-07-29 21:22:43 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,33 @@
 class Solution {
 
-    public int helper(int n,int []dp){
-        if(n==1 || n == 0){
-            dp[n]=1;
-            return dp[n];
-        }
+    // public int helper(int n,int []dp){
+    //     if(n==1 || n == 0){
+    //         dp[n]=1;
+    //         return dp[n];
+    //     }
 
-        if(dp[n]!=-1)
-            return dp[n];
+    //     if(dp[n]!=-1)
+    //         return dp[n];
 
-        dp[n]=helper(n-1,dp)+helper(n-2,dp);
+    //     dp[n]=helper(n-1,dp)+helper(n-2,dp);
 
-        return dp[n];
-    }
+    //     return dp[n];
+    // }
 
     public int climbStairs(int n) {
-        int []dp = new int[n+1];
+        // int []dp = new int[n+1];
         
-        Arrays.fill(dp,-1);
-        helper(n,dp);
-
-        return dp[n];
+        // Arrays.fill(dp,-1);
+        // // // helper(n,dp);
+        // dp[0]=1;
+        // dp[1]=1;
+        int prev = 1;
+        int curr = 1;
+        for(int i=2;i<n+1;i++){
+            int temp = prev;
+            prev = curr;
+            curr = temp + curr;
+        }
+        return curr;
     }
 }
