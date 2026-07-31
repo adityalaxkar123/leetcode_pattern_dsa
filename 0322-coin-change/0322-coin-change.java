2c5d8af412dcf83268385e8664c9295fbd663379
adityalaxkar <adityalaxkar9876@gmail.com>
2026-07-31 17:42:47 +0530

    Time: 17 ms (48.3%), Space: 46.7 MB (37.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,36 +1,57 @@
 class Solution {
 
-    public int helper(int []coin,int amount,int idx,int [][]dp){
-        if(amount==0)
-            return 0;
-        if(idx==0){
-            if(amount%coin[idx]==0)
-                return amount/coin[idx];
-            return Integer.MAX_VALUE;
-        }
-        if(dp[idx][amount]!=-1)
-            return dp[idx][amount];
-        int pick=Integer.MAX_VALUE;
-        if(amount>=coin[idx]){
-            pick=helper(coin,amount-coin[idx],idx,dp);
-            if(pick!=Integer.MAX_VALUE)
-                pick+=1;
-        }
-        int nopick = helper(coin,amount,idx-1,dp);
-        dp[idx][amount]=Math.min(pick,nopick);
-        return dp[idx][amount];
-    }
+    // public int helper(int[] coin, int amount, int idx, int[][] dp) {
+    //     if (amount == 0)
+    //         return 0;
+    //     if (idx == 0) {
+    //         if (amount % coin[idx] == 0)
+    //             return amount / coin[idx];
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if (dp[idx][amount] != -1)
+    //         return dp[idx][amount];
+    //     int pick = Integer.MAX_VALUE;
+    //     if (amount >= coin[idx]) {
+    //         pick = helper(coin, amount - coin[idx], idx, dp);
+    //         if (pick != Integer.MAX_VALUE)
+    //             pick += 1;
+    //     }
+    //     int nopick = helper(coin, amount, idx - 1, dp);
+    //     dp[idx][amount] = Math.min(pick, nopick);
+    //     return dp[idx][amount];
+    // }
 
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int [][]dp = new int[n][amount+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        if(amount==0)
+        int[][] dp = new int[n][amount + 1];
+        // for(int i=0;i<n;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        if (amount == 0)
             return 0;
         // Arrays.sort(coins);
-        int ans = helper(coins,amount,n-1,dp);
-        return ans==Integer.MAX_VALUE?-1:ans;
+        // int ans = helper(coins,amount,n-1,dp);
+        if (amount == 0)
+            return 0;
+        for (int i = 1; i <= amount; i++) {
+            if (i%coins[0] == 0)
+                dp[0][i] = i/coins[0];
+            else
+                dp[0][i] = Integer.MAX_VALUE;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                int pick = Integer.MAX_VALUE;
+                if (j >= coins[i]) {
+                    pick = dp[i][j-coins[i]];
+                    if (pick != Integer.MAX_VALUE)
+                        pick += 1;
+                }
+                int nopick = dp[i-1][j];
+                dp[i][j] = Math.min(pick, nopick);
+            }
+        }
+
+        return dp[n-1][amount] == Integer.MAX_VALUE ? -1 : dp[n-1][amount];
     }
 }
