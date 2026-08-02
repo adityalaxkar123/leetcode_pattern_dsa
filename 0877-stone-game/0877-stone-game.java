1b436e70431b3c77dcc9f2565519dd47025fdf2c
adityalaxkar <adityalaxkar9876@gmail.com>
2026-08-02 09:38:06 +0530

    Time: 7 ms (21.79%), Space: 46.7 MB (16.42%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,22 +1,32 @@
 class Solution {
 
-    public int helper(int []nums,int st,int end,int [][]dp){
-        if(st==end)
+    public int helper(int[] nums, int st, int end, int[][] dp) {
+        if (st == end)
             return nums[st];
-        if(dp[st][end]!=-1)
+        if (dp[st][end] != -1)
             return dp[st][end];
-        int leftPick = nums[st] - helper(nums,st+1,end,dp);
-        int rightPick = nums[end] - helper(nums,st,end-1,dp);
-        dp[st][end]=Math.max(leftPick,rightPick);
+        int leftPick = nums[st] - helper(nums, st + 1, end, dp);
+        int rightPick = nums[end] - helper(nums, st, end - 1, dp);
+        dp[st][end] = Math.max(leftPick, rightPick);
         return dp[st][end];
     }
 
     public boolean stoneGame(int[] piles) {
         int n = piles.length;
-        int [][]dp = new int[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
         }
-        return helper(piles,0,n-1,dp)>0;
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = i+1; j < n; j++) {
+                int leftPick = piles[i] - dp[i+1][j];
+                int rightPick = piles[j] - dp[i][j-1];
+                dp[i][j] = Math.max(leftPick, rightPick);
+            }
+        }
+
+        // return helper(piles,0,n-1,dp)>0;
+        return dp[n - 1][n-1]>0;
     }
 }
