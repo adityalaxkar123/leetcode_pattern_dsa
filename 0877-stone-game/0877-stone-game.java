2a2bf433ffcb1636791a7036b9f6b419fbe430ae
adityalaxkar <adityalaxkar9876@gmail.com>
2026-08-02 09:33:12 +0530

    Time: 8 ms (18.78%), Space: 46.9 MB (10.85%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,22 @@
 class Solution {
+
+    public int helper(int []nums,int st,int end,int [][]dp){
+        if(st==end)
+            return nums[st];
+        if(dp[st][end]!=-1)
+            return dp[st][end];
+        int leftPick = nums[st] - helper(nums,st+1,end,dp);
+        int rightPick = nums[end] - helper(nums,st,end-1,dp);
+        dp[st][end]=Math.max(leftPick,rightPick);
+        return dp[st][end];
+    }
+
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int [][]dp = new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return helper(piles,0,n-1,dp)>0;
     }
 }
