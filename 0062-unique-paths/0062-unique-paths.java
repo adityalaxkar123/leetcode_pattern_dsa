611e52e3d6364f9d694935dcf2546bcc0637f369
adityalaxkar <adityalaxkar9876@gmail.com>
2026-08-03 21:18:04 +0530

    Time: 1 ms (14.4%), Space: 42.1 MB (48.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,28 +1,43 @@
 class Solution {
 
-    public int helper(int [][]dp,int i,int j){
+    public int helper(int[][] dp, int i, int j) {
         int m = dp.length;
         int n = dp[0].length;
-        if(i>=m)
+        if (i >= m)
             return 0;
-        if(j>=n)
+        if (j >= n)
             return 0;
-        if(i==m-1 && j==n-1){
-            dp[i][j]=1;
+        if (i == m - 1 && j == n - 1) {
+            dp[i][j] = 1;
             return dp[i][j];
         }
-        if(dp[i][j]!=-1)
+        if (dp[i][j] != -1)
             return dp[i][j];
-        int right = helper(dp,i,j+1);
-        int bottom = helper(dp,i+1,j);
-        dp[i][j] = right+bottom;
+        int right = helper(dp, i, j + 1);
+        int bottom = helper(dp, i + 1, j);
+        dp[i][j] = right + bottom;
         return dp[i][j];
     }
 
     public int uniquePaths(int m, int n) {
-        int [][]dp = new int[m][n];
-        for(int i=0;i<m;i++)
-            Arrays.fill(dp[i],-1);
-        return helper(dp,0,0);
+        // int[][] dp = new int[m][n];
+        int []next = new int[n];
+        next[n - 1] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            int []curr = new int[n];
+            curr[n-1]=1;
+            for (int j = n - 1; j >= 0; j--) {
+                if(i==m-1 && j==n-1)
+                    continue;
+                int right=0,bottom=0;
+                if(j+1<n)
+                    right = curr[j+1];
+                if(i+1<m)
+                    bottom = next[j];
+                curr[j] = right + bottom;
+            }
+            next=curr;
+        }
+        return next[0];
     }
 }
