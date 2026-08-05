477ad0633d2ec0a9356310c5b5748fa120509369
adityalaxkar <adityalaxkar9876@gmail.com>
2026-08-05 16:16:03 +0530

    Time: 3 ms (79.82%), Space: 45.4 MB (75.86%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,23 +1,38 @@
 class Solution {
 
-    public int helper(List<List<Integer>> triangle,int i,int j,int [][]dp){
+    public int helper(List<List<Integer>> triangle, int i, int j, int[][] dp) {
         int n = triangle.size();
-        if(i==n-1)
+        if (i == n - 1)
             return triangle.get(i).get(j);
-        if(dp[i][j]!=Integer.MAX_VALUE)
+        if (dp[i][j] != Integer.MAX_VALUE)
             return dp[i][j];
-        int down = helper(triangle,i+1,j,dp);
-        int diagonal = helper(triangle,i+1,j+1,dp);
-        dp[i][j] = triangle.get(i).get(j) + Math.min(down,diagonal);
+        int down = helper(triangle, i + 1, j, dp);
+        int diagonal = helper(triangle, i + 1, j + 1, dp);
+        dp[i][j] = triangle.get(i).get(j) + Math.min(down, diagonal);
 
         return dp[i][j];
     }
 
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int [][]dp = new int[n][n];
-        for(int i=0;i<n;i++)
-            Arrays.fill(dp[i],Integer.MAX_VALUE);
-        return helper(triangle,0,0,dp);
+        // int [][]dp = new int[n][n];
+        int[] next = new int[n];
+        for(int i=0;i<n;i++){
+            next[i]=triangle.get(n-1).get(i);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            int []curr = new int[n];
+            for (int j = i; j >= 0; j--) {
+                if(i==n-1){
+                    curr[j]=triangle.get(i).get(j);
+                    continue;
+                }
+                int down = next[j];
+                int diagonal = next[j+1];
+                curr[j] = triangle.get(i).get(j) + Math.min(down, diagonal);
+            }
+            next=curr;
+        }
+        return next[0];
     }
 }
