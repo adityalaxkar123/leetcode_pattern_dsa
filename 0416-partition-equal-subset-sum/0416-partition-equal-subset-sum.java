bc81aadcb0c9f9e8f74329ca21cff9efe835c54b
adityalaxkar <adityalaxkar9876@gmail.com>
2026-08-06 20:43:03 +0530

    Time: 50 ms (47.68%), Space: 47.1 MB (62.59%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,35 +1,33 @@
 class Solution {
-
-    public boolean helper(int []nums,int idx,int sum,int [][]dp){
-        if(sum==0)
-            return true;
-        if(idx==0){
-            if(sum-nums[idx]==0)
-                return true;
-            return false;
-        }
-        if(dp[idx][sum]!=0){
-            return (dp[idx][sum]==1)?true:false;
-        }
-        boolean pick = false,nopick = false;
-        if(sum-nums[idx]>=0)
-            pick = helper(nums,idx-1,sum-nums[idx],dp);
-        nopick = helper(nums,idx-1,sum,dp);
-        dp[idx][sum]=(pick || nopick)?1:2;
-
-        return (dp[idx][sum]==1)?true:false;
-    }
-
     public boolean canPartition(int[] nums) {
         int total = 0;
-        for(int i : nums){
-            total+=i;
+        for (int i : nums) {
+            total += i;
         }
         int n = nums.length;
-        if(total%2!=0)
+        if (total % 2 != 0)
             return false;
-        int [][]dp = new int[n][total/2 + 1];
+        int[]prev = new int[total / 2 + 1];
+        prev[0] = 1;
+        for (int i = 1; i <= total / 2; i++) {
+            if (i - nums[0] == 0)
+                prev[i] = 1;
+            else
+                prev[i] = 2;
+        }
 
-        return helper(nums,n-1,total/2,dp);
+        for (int i = 1; i < n; i++) {
+            int []curr = new int[total/2 + 1];
+            for (int j = 1; j <= total / 2; j++) {
+                boolean pick = false, nopick = false;
+                if (j - nums[i] >= 0)
+                    pick = (prev[j - nums[i]]==1)?true:false;
+                nopick = (prev[j]==1)?true:false;
+                curr[j] = (pick || nopick) ? 1 : 2;
+            }
+            prev = curr;
+        }
+
+        return (prev[total/2]==1)?true:false;
     }
 }
