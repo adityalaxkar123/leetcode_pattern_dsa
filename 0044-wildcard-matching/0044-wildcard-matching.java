661a5197decab122c64e64c8d6f0480cc77680c8
adityalaxkar <adityalaxkar9876@gmail.com>
2026-08-09 11:55:59 +0530

    Time: 53 ms (27.4%), Space: 47.4 MB (63.29%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,23 +2,10 @@ class Solution {
     public boolean isMatch(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        // int [][]dp = new int[n+1][m+1];
         int []prev = new int[m+1];
         prev[0]=1;
         for(int j=1;j<m+1;j++){
-            int k = j;
-            boolean ans = true;
-            while(k>0){
-                if(s2.charAt(k-1)!='*'){
-                    ans=false;
-                    break;
-                }
-                k--;
-            }
-            if(ans)
-                prev[j]=1;
-            else
-                prev[j]=2;
+            prev[j]=(prev[j-1]==1 && s2.charAt(j-1)=='*')?1:2;
         }
 
         for(int i=1;i<n+1;i++){
