c589f4b8c7cd3aa5517cd3bca1a5900e008f2a71
adityalaxkar <adityalaxkar9876@gmail.com>
2026-08-09 11:52:43 +0530

    Time: 57 ms (16.01%), Space: 47.1 MB (68.35%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,42 +1,10 @@
 class Solution {
-
-    public boolean helper(String s1, String s2, int i, int j, int[][] dp) {
-        if (i < 0) {
-            int k = j;
-            while (k >= 0) {
-                if (s2.charAt(k) != '*')
-                    return false;
-                k--;
-            }
-            return true;
-        }
-        if (j < 0) {
-            if (i >= 0)
-                return false;
-            return true;
-        }
-        if (dp[i][j] != 0)
-            return (dp[i][j] == 1) ? true : false;
-
-        boolean match = false, notMatch = false;
-        if (s1.charAt(i) == s2.charAt(j) || s2.charAt(j) == '?')
-            match = helper(s1, s2, i - 1, j - 1, dp);
-        else if (s2.charAt(j) == '*')
-            notMatch = helper(s1, s2, i - 1, j, dp) || helper(s1, s2, i, j - 1, dp);
-        else
-            return false;
-        dp[i][j] = (match || notMatch) ? 1 : 2;
-        return (dp[i][j] == 1) ? true : false;
-    }
-
     public boolean isMatch(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        int [][]dp = new int[n+1][m+1];
-        for(int i=1;i<n+1;i++){
-            dp[i][0]=2;
-        }
-        dp[0][0]=1;
+        // int [][]dp = new int[n+1][m+1];
+        int []prev = new int[m+1];
+        prev[0]=1;
         for(int j=1;j<m+1;j++){
             int k = j;
             boolean ans = true;
@@ -48,23 +16,26 @@ class Solution {
                 k--;
             }
             if(ans)
-                dp[0][j]=1;
+                prev[j]=1;
             else
-                dp[0][j]=2;
+                prev[j]=2;
         }
 
         for(int i=1;i<n+1;i++){
+            int []curr = new int[m+1];
+            curr[0]=2;
             for(int j=1;j<m+1;j++){
                 int match = 2,notMatch=2;
-                dp[i][j]=2;
+                curr[j]=2;
                 if(s1.charAt(i-1)==s2.charAt(j-1) || s2.charAt(j-1)=='?')
-                    match = dp[i-1][j-1];
+                    match = prev[j-1];
                 else if(s2.charAt(j-1)=='*')
-                    notMatch = (dp[i-1][j]==1 || dp[i][j-1]==1)?1:2;
-                dp[i][j] = (match==1 || notMatch==1)?1:2;
+                    notMatch = (prev[j]==1 || curr[j-1]==1)?1:2;
+                curr[j] = (match==1 || notMatch==1)?1:2;
             }
+            prev = curr;
         }
        
-       return dp[n][m]==1?true:false;
+       return prev[m]==1?true:false;
     }
 }
