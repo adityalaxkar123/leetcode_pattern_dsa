0f93d03bbccc979e686ec6be9081aad94609526d
adityalaxkar <adityalaxkar9876@gmail.com>
2026-08-19 21:50:42 +0530

    Time: 2329 ms (5.61%), Space: 43.3 MB (38.92%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,15 +1,15 @@
 class Solution {
 
-    public boolean helper(String s1, String s2, String s3,int i,int j,int k){
-        if(k<0)
+    public boolean helper(String s1, String s2, String s3,int i,int j){
+        if(i<0 && j<0)
             return true;
         boolean pick = false;
-        if(i>=0 && s1.charAt(i)==s3.charAt(k) && j>=0 && s2.charAt(j)==s3.charAt(k)){
-            pick = helper(s1,s2,s3,i-1,j,k-1) || helper(s1,s2,s3,i,j-1,k-1);
-        }else if(i>=0 && s1.charAt(i)==s3.charAt(k)){
-            pick = helper(s1,s2,s3,i-1,j,k-1);
-        }else if(j>=0 && s2.charAt(j)==s3.charAt(k)){
-            pick = helper(s1,s2,s3,i,j-1,k-1);
+        if(i>=0 && s1.charAt(i)==s3.charAt(i+j+1) && j>=0 && s2.charAt(j)==s3.charAt(i+j+1)){
+            pick = helper(s1,s2,s3,i-1,j) || helper(s1,s2,s3,i,j-1);
+        }else if(i>=0 && s1.charAt(i)==s3.charAt(i+j+1)){
+            pick = helper(s1,s2,s3,i-1,j);
+        }else if(j>=0 && s2.charAt(j)==s3.charAt(i+j+1)){
+            pick = helper(s1,s2,s3,i,j-1);
         }else{
             return false;
         }
@@ -21,8 +21,8 @@ class Solution {
         int n = s1.length();
         int m = s2.length();
         int k = s3.length();
-        if(k<n+m)
+        if(k!=n+m)
             return false;
-        return helper(s1,s2,s3,n-1,m-1,k-1); 
+        return helper(s1,s2,s3,n-1,m-1); 
     }
 }
